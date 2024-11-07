a00a89ee58b99325f1c6ff4a5979cc53f753aa01
VBSDK <125219878+VBSDK@users.noreply.github.com>
2024-11-07 21:24:18 +0100

    Update and rename header.c to header.h

---
+++ src/header.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_H
+#define HEADER_H
+// Tilføj dette: #include "header.h" i alle filer.
+
+//Her kan indsættes konstante definitioner som f.eks. #DEFINE PI 3.14...
+
+//Her indsættes prototyper af ALLE funktioner der laves i seperate dokumenter.
+// - Det er ikke nødvendigt at indsætte prototyperne i en bestemt rækkefølge. Men organiser dem så man kan finde rundt i dem.
+
+
+
+
+#endif HEADER_H

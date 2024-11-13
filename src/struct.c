fc40737de77fdbea5d7d7f4b1a80bfd82a59ed63
Theresa <theresatran005@gmail.com>
2024-11-13 13:43:39 +0100

    Tilføjet en elevprofil

--- src/struct.c
+++ src/struct.c
@@ -4,5 +4,6 @@ void getStruct(){
     student_profile adam = {"Adam",7.45, "STX", "Samfundsfag"};
     student_profile mathilde = {"Mathilde", 11.11, "HTX", "Matematik"};
     student_profile oliver = {"Oliver", 5.46, "HHX", "Virksomhedsoekonomi"};
+    student_profile gertrud = {"Gertrud", 8.78, "STX", "Biologi"};
 
 }

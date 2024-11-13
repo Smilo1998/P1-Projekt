c504b3f77495707563c1bf02fe6312caa86ac770
Sebastian Milo <sebatianjmilo@outlook.com>
2024-11-13 12:58:35 +0100

    Initialisering af struct i headerfil og erklæring af de første elevprofiler

--- include/header.h
+++ include/header.h
@@ -1,12 +1,24 @@
 #ifndef HEADER_H
 #define HEADER_H
-// Tilføj dette: #include "header.h" i alle filer.
 
-//Her kan indsættes konstante definitioner som f.eks. #DEFINE PI 3.14...
+#define NAME_LENGTH 30
+#define SUBJECT_NAME
+typedef struct elevprofil{
+    char name[NAME_LENGTH + 1];
+    double gpa;
+    char school_type;
+    char yndlingsfag[SUBJECT_NAME + 1];
+} elevprofil;
 
-//Her indsættes prototyper af ALLE funktioner der laves i seperate dokumenter.
-// - Det er ikke nødvendigt at indsætte prototyperne i en bestemt rækkefølge. Men organiser dem så man kan finde rundt i dem.
+// Tilfoej dette: #include "header.h" i alle filer.
 
+//Her kan indsaettes konstante definitioner som f.eks. #DEFINE PI 3.14...
+
+//Her indsaettes prototyper af ALLE funktioner der laves i seperate dokumenter.
+// - Det er ikke noedvendigt at indsaette prototyperne i en bestemt raekkefoelge. Men organiser dem saa man kan finde rundt i dem.
+
+// Funktioner fra Struct.c
+void getStruct();
 
 
 
--- src/struct.c
+++ src/struct.c
@@ -1,1 +1,9 @@
-//her laves vores struct
+#include "header.h"
+
+void getStruct(){
+    elevprofil adam = {"Adam", "STX", 7.45, "Samfundsfag"};
+    elevprofil mathilde = {"Mathilde", "HTX", 11.11, "Matematik"};
+    elevprofil lauritz = {"Lauritz", "HHX", 4.31, "Idraet"};
+    elevprofil cornelius = {"Cornelius", "HF", 5.67, "Musik"};
+    elevprofil sebastian = {"Sebastian", "HHX", 7.83, "Samfundsfag"};
+}

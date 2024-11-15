86d71f6600ed8b5009191fb3d1a7befe6df86466
Victor Sondergaard <victor@ms24.dk>
2024-11-15 13:54:57 +0100

    Ændring i input. boolean fagvalg

--- src/input.c
+++ src/input.c
@@ -1,24 +1,22 @@
 #include "../include/header.h" 
 
-// Funktion som tager input fra bruger og gemmer gpa og favorite subject i et struct student_profile
+// Funktion som tager input fra bruger og gemmer gpa og fag-boolean værdi i et struct student_profile
 student_profile addStudent(){
         student_profile user_profile;
 
         printf("Enter your grade average: ");
         scanf("%lf", &user_profile.gpa);
 
-        subjectInput();
-
-        user_profile.fag_array;
+        user_profile.fag_array = subjectInput();
 
         return user_profile;
     }  
 
-
-int *subjectInput(){
+//Spørger efter hvert fag og svaret modtages i 1 eller 0 for at gøre det nemmere i fremtiden. kan altid ændres.
+int subjectInput(){
     int current_subject, i;
-    int subjects_print[10];
-    int subjects_choice[10];
+    char* subjects_print[10];
+
     subjects_print[0] = "Matematik";
     subjects_print[1] = "Fysik";
     subjects_print[2] = "Kemi";
@@ -30,10 +28,18 @@ int *subjectInput(){
     subjects_print[8] = "Virksomhedsoekonomi";
     subjects_print[9] = "Afsaetning";
     
+    //alloker plads til subjects choice; //FREE PLADSEN SENERE I PROGRAMMET!
+    int *subjects_choice = (int*) malloc(10*sizeof(int));
+
+    if(subjects_choice == NULL)
+        printf("ALLOCATION OF MEMORY ERROR");
+        exit(EXIT_FAILURE);
+
     int i = 0;
     for(i = 0; i < 10; i++){
+        printf("Type 1 if you completed following subjects, 0 if you have not completed the subject.\n");
         printf("%s: ", subjects_print[i]);
-        scanf("%s", &subjects_choice[i]);
+        scanf("%d", &subjects_choice[i]);
     }
 
     return *subjects_choice;

fbffe1403acad6fdf37e6303e84b6cb240f9f58b
Victor Sondergaard <victor@ms24.dk>
2024-11-21 13:04:21 +0100

    Output start victor

--- src/output.c
+++ src/output.c
@@ -1,3 +1,18 @@
-//Dette er filen til output
-// Theresa test
-// Theresa test 2
+#include "../include/header.h"
+
+void printEducation(student_profile student, educations educationData){
+    int i, city;
+    printf("Recommended education based on your inputs:");
+    printf("\n%s\n", *student.education_choice.name);
+    printf("Cities with this education:\n");
+        for(i = 0; i < MAX_CITY; i++){
+            city = student.education_choice.city[i];
+            printf("%d", i);
+        }
+        
+
+
+    printf("\nUse this link to read more:\n%s\n", *student.education_choice.link);
+
+
+}
